82bc3b8ea4438dc958413af8d640c1e7f6210989
fuqskg <106498001+fuqskg@users.noreply.github.com>
2022-08-23 17:44:46 +0900

    java_예외처리

---
+++ chap14/src/sec01_typeofexception/EX02_UnCheckedException/UnCheckedException.java
@@ -0,0 +1,30 @@
+package sec01_typeofexception.EX02_UnCheckedException;
+
+class A{}
+class B extends A{}
+
+public class UnCheckedException {
+
+	public static void main(String[] args) {
+		//UncheckedException = RuntimeException (실행예외)
+		
+		//1. ArithmeticException
+		System.out.println(3/0);
+		
+		//2. ClassCastException
+		A a = new A();
+		B b = (B)a;
+		
+		//3. ArrayIndexOutOfBoundsExceiption
+		int[] array = {1, 2, 3};
+		System.out.println(array[3]);
+		
+		//4. NumberFormatException
+		int num = Integer.parseInt("10!");
+		
+		//5. NullPointerException
+		String str = null;
+		System.out.println(str.charAt(2));
+	}
+
+}
---
+++ chap14/src/sec02_Exceptionhandlingsyntax/EX01_TryCatchFinally/TryCatchFinally.java
@@ -0,0 +1,26 @@
+package sec02_Exceptionhandlingsyntax.EX01_TryCatchFinally;
+
+public class TryCatchFinally {
+
+	public static void main(String[] args) {
+		// 1. try-catch
+		try {
+			System.out.println(3 / 0);// 실행예외
+			System.out.println("프로그램 종료");
+		} catch (ArithmeticException e) {
+			System.out.println("숫자는 0으로 나눌 수 없습니다.");
+			System.out.println("프로그램 종료");
+		}
+		
+		// 2. try-catch-finally
+		try {
+			System.out.println( 3 / 0); // 실행예외
+		} catch (ArithmeticException e) {
+			System.out.println("숫자는 0으로 나눌 수 없습니다.");
+		}finally {
+			System.out.println("프로그램 종료");
+		}
+
+	}
+
+}
